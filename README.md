1afc47cb291743ac0c05ed94a2da6caa0d3885b5
xingxing <xxx823952375@gmail.com>
2016-11-03 23:30:02 +0800

    add pic

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DragFrameLayout
 一个可以向上拖动view的DragFrameLayout,仅仅只是demo
+
+## Screenshot
+
+![Screenshot](art/ic_drag.png)
--- app/src/main/java/xingstarx/com/dragframelayout/view/DragFrameLayout.java
+++ app/src/main/java/xingstarx/com/dragframelayout/view/DragFrameLayout.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
